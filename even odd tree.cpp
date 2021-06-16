877f26667ea7c725fb67215c63a3ba1618d55b05
Khushi Gangopadhyay <khushiganguly08@gmail.com>
2021-06-16 23:18:08 +0530

    Day 40 of #100DaysofCode

---
+++ even odd tree.cpp
@@ -0,0 +1,28 @@
+//problem link:https://leetcode.com/problems/even-odd-tree/
+
+
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int level= 0;
+        while (q.size()) {
+            int count = q.size(), prev = level % 2 == 0 ? 0 : INT_MAX;
+            while (count--) {
+                auto node = q.front();
+                q.pop();
+                if (level % 2 == 0) {
+                    if (node->val % 2 == 0 || node->val <= prev) return false;
+                } else {
+                    if (node->val % 2 || node->val >= prev) return false;
+                }
+                prev = node->val;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            ++level;
+        }
+        return true;
+    }
+};
