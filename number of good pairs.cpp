8335a3dfb5b498038208fc16fb68e8f7a0774cbe
Khushi Gangopadhyay <khushiganguly08@gmail.com>
2021-06-21 23:29:53 +0530

    Day 45 of #100DaysofCode

---
+++ number of good pairs.cpp
@@ -0,0 +1,12 @@
+//problem link:https://leetcode.com/problems/number-of-good-pairs/
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int> map;
+        int res=0;
+        for(int i:nums)
+            res+=map[i]++;
+        return res;
+    }
+};
