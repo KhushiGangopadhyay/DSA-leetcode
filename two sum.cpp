663f77e92f468618df3338eb97716440ef75ee7d
Khushi Gangopadhyay <khushiganguly08@gmail.com>
2021-07-07 23:40:47 +0530

    Day 61 of #100DaysofCode

---
+++ two sum.cpp
@@ -0,0 +1,19 @@
+//problem link:https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash; 
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++)
+            if (hash.count(target - nums[i])) 
+            {
+                result.push_back(hash[target - nums[i]]);
+                result.push_back(i);
+                return result;
+            }
+            else 
+                hash[nums[i]] = i;
+        return result;
+    }
+};
